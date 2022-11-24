21026e1c08ce59649a323f7c3bc7895b38364534
duncan.macleod <duncan.macleod@ligo.org>
2022-11-24 11:49:48 +0000

    fix duplicate installation of libclient.so

--- recipe/install-base-monitors.sh
+++ recipe/install-base-monitors.sh
@@ -3,4 +3,10 @@
 pushd _build
 set -ex
 
-make -j ${CPU_COUNT} V=1 VERBOSE=1 -C Services install-exec-am install-lib_pkgconfigDATA
+# build the Services subdir, but not libclient.so (part of gds-base-crtools)
+make -j ${CPU_COUNT} V=1 VERBOSE=1 \
+  -C Services \
+  install-exec-am \
+  install-lib_pkgconfigDATA \
+  lib_LTLIBRARIES="libmonitor.la libtclient.la" \
+;
